e61f92f989cd3368f4409504c5aed43902863011
example <alisayar0107@gmail.com>
2021-04-08 15:49:32 +0300

    eventlisteners dynamically added to the project

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
+
+function sendNumberValue(number) {
+  return console.log(number);
+}
+
+// Add Event Listeners for numbers, operators, decimal buttons.
+
+inputBtns.forEach((inputBtn) => {
+  // If is number buttons because they don't have a class.
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains('operator')) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains('decimal')) {
+    inputBtn.addEventListener('click', () => sendNumberValue());
+  }
+});
